f542744a7e58b25f5f5dc4b10a16357fcebec58d
LoopFruits <Kensworldc@gmail.com>
2022-04-24 04:47:06 -0400

    experimenting with Zelpin integration

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    // experimenting with Zelpin 
+    <div class="Card">
+      <div class="AnyPay-Home">
+        <span class="Hi-Danielle">
+          Hi Danielle!
+        </span>
+          <div class="tab-bar"></div>
+      </div>
     </div>
   );
 }
